5faca5e4fb8af42e518977edfe421a499e0ce815
zabuzaff <zabuzaff@gmail.com>
2023-05-16 17:30:38 +0800

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $datas = User::paginate(10);
+
+        return view('user.manage', compact('datas'));
+    }
+
+    public function edit($id)
+    {
+        $data = User::find($id)->toArray();
+
+        return view('user.edit', compact('data'));
+    }
+
+    public function destroy($property)
+    {
+        User::find($property)->delete();
+
+        return response()->json(['success' => true]);
+    }
+
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->merge([
+            'password' => bcrypt('password')
+        ]);
+
+        User::create($request->all());
+
+        return redirect()->route('user.index')
+            ->with('success', "User Successfully Added");
+    }
+
+    public function update(Request $request, $property)
+    {
+        User::find($property)->update($request->all());
+
+        return redirect()->route('user.index')
+            ->with('success', "User Successfully Updated");
+    }
+}
