c29c859e1fa46bebd9b631cee750d9c59eab9938
zabuzaff <zabuzaff@gmail.com>
2023-06-14 01:25:20 +0800

    update

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -34,7 +34,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ Route::currentRouteName() == 'post.index' ? 'active' : '' }}"
+                    <a class="nav-link {{ Route::currentRouteName() == 'complaint.index' ? 'active' : '' }}"
                         href="{{ route('complaint.index') }}">
                         <div
                             class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
--- routes/web.php
+++ routes/web.php
@@ -47,9 +47,8 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/complaints/report', [ComplaintController::class, 'report'])->name('complaint.report');
     Route::get('/dashboard', [HomeController::class, 'index'])->name('home');
     Route::get('/form-example', [HomeController::class, 'formExample'])->name('form-example');
-    Route::get('/{page}', [PageController::class, 'index'])->name('page');
+    // Route::get('/{page}', [PageController::class, 'index'])->name('page');
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-
 
     Route::resources([
         'user' => UserController::class,
@@ -59,6 +58,7 @@ Route::group(['middleware' => 'auth'], function () {
         'comment' => CommentController::class,
     ]);
 });
+
 
 
 URL::forceScheme('https');
